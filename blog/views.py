5b065a01b7c430d1fbc5bb20e4450dbefae4663a
zara <zarazolfaghari76@gmail.com>
2020-07-27 10:34:30 +0430

    test views.py with templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse, JsonResponse
+
+
+def index(request):
+    return HttpResponse("some thing")
+
+
+def json(request):
+    data = {
+        "1": {
+            "name": "st1",
+            "mode": "sad"
+        },
+        "2": {
+            "name": "st2",
+            "mode": "happy"
+        }
+    }
+    return JsonResponse(data)
+
+
+def template(request):
+    context = {
+
+          "students": [
+              {
+                  "name": "st1",
+                  "mode": "dep"
+              },
+              {
+                  "name": "st2",
+                  "mode": "sad"
+              },
+              {
+                  "name": "st3",
+                  "mode": "happy"
+              },
+          ]
+
+
+    }
+    return render(request, 'home.html', context)
