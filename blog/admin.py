68f84c562e8f6e306c178534ffa3cfabba67514e
zara <zarazolfaghari76@gmail.com>
2020-07-27 12:20:55 +0430

    admin panel managment

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Article
 
-admin.site.register(Article)
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status')
+    list_filter = ('publish', 'status')
+    search_fields = ('title', 'description')
+    prepopulated_fields = {'slug': ('title',)}
+    ordering = ['status', 'publish']
+
+
+admin.site.register(Article, ArticleAdmin)
