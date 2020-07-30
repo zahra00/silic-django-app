f8d45fb4d229ab96501fdf59bb31bf6349bcc6a0
zara <zarazolfaghari76@gmail.com>
2020-07-30 15:28:26 +0430

    add category model

--- blog/admin.py
+++ blog/admin.py
@@ -1,13 +1,27 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Category
+
+
+class CategoryeAdmin(admin.ModelAdmin):
+    list_display = ('position', 'title', 'slug', 'status')
+    list_filter = ('status',)
+    search_fields = ('title', 'description')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Category, CategoryeAdmin)
 
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'status')
+    list_display = ('title', 'slug', 'status', 'category_to_str')
     list_filter = ('publish', 'status')
     search_fields = ('title', 'description')
     prepopulated_fields = {'slug': ('title',)}
     ordering = ['status', 'publish']
+
+    def category_to_str(self, obj):
+        return "، ".join([category.title for category in obj.category.all()])
+    category_to_str.short_description = "دسته‌بندی"
 
 
 admin.site.register(Article, ArticleAdmin)
--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,21 @@ from django.db import models
 from django.utils import timezone
 
 
+class Category(models.Model):
+    title = models.CharField(max_length=100, verbose_name="دسته‌بندی")
+    slug = models.SlugField(max_length=100, unique=True, allow_unicode=True, verbose_name="آدرس دسته‌بندی")
+    status = models.BooleanField(default=True, verbose_name="آیا نمایش داده شود؟")
+    position = models.IntegerField(verbose_name="پوزیشن")
+
+    class Meta:
+        verbose_name = "دسته‌بندی"
+        verbose_name_plural = "دسته‌بندی‌ها"
+        ordering = ['position']
+
+    def __str__(self):
+        return self.title
+
+
 class Article(models.Model):
     STATUS_CHOICE = (
         ('d', 'پیش‌نویس'),
@@ -9,7 +24,8 @@ class Article(models.Model):
     )
 
     title = models.CharField(max_length=100, verbose_name="عنوان")
-    slug = models.SlugField(max_length=100, unique=True, allow_unicode=True, verbose_name="عنوان مقاله")
+    slug = models.SlugField(max_length=100, unique=True, allow_unicode=True, verbose_name="آدرس مقاله")
+    category = models.ManyToManyField(Category)
     description = models.TextField(verbose_name="متن")
     thumbnail = models.ImageField(upload_to='images')
     publish = models.DateTimeField(default=timezone.now)
